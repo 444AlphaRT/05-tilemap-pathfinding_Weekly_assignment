95ee5e4141cf9290e4c9b2c602f66a6f34aad4da
rb <reutbechor@gmail.com>
2025-12-06 20:44:13 +0200

    Add files via upload

---
+++ Assets/Scripts/0-bfs/0-bfs/BFS.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+/**
+ * A generic implementation of the BFS algorithm.
+ * @author Erel Segal-Halevi
+ * @since 2020-02
+ */
+public class BFS {
+    public static void FindPath<NodeType>(
+            IGraph<NodeType> graph, 
+            NodeType startNode, NodeType endNode, 
+            List<NodeType> outputPath, int maxiterations=1000)
+    {
+        Queue<NodeType> openQueue = new Queue<NodeType>();
+        HashSet<NodeType> openSet = new HashSet<NodeType>();
+        Dictionary<NodeType, NodeType> previous = new Dictionary<NodeType, NodeType>();
+        openQueue.Enqueue(startNode);
+        openSet.Add(startNode);
+        int i; for (i = 0; i < maxiterations; ++i) { // After maxiterations, stop and return an empty path
+            if (openQueue.Count == 0) {
+                break;
+            } else {
+                NodeType searchFocus = openQueue.Dequeue();
+
+                if (searchFocus.Equals(endNode)) {
+                    // We found the target -- now construct the path:
+                    outputPath.Add(endNode);
+                    while (previous.ContainsKey(searchFocus)) {
+                        searchFocus = previous[searchFocus];
+                        outputPath.Add(searchFocus);
+                    }
+                    outputPath.Reverse();
+                    break;
+                } else {
+                    // We did not found the target yet -- develop new nodes.
+                    foreach (var neighbor in graph.Neighbors(searchFocus)) {
+                        if (openSet.Contains(neighbor)) {
+                            continue;
+                        }
+                        openQueue.Enqueue(neighbor);
+                        openSet.Add(neighbor);
+                        previous[neighbor] = searchFocus;
+                    }
+                }
+            }
+        }
+    }
+
+    public static List<NodeType> GetPath<NodeType>(IGraph<NodeType> graph, NodeType startNode, NodeType endNode, int maxiterations=1000) {
+        List<NodeType> path = new List<NodeType>();
+        FindPath(graph, startNode, endNode, path, maxiterations);
+        return path;
+    }
+    /*
+     * Counts the total number of unique nodes reachable from the startNode.
+     * Used to validate map connectivity.
+     */
+    public static int CountReachableNodes<NodeType>(
+            IGraph<NodeType> graph,
+            NodeType startNode,
+            int maxiterations = 10000)
+    {
+        Queue<NodeType> openQueue = new Queue<NodeType>();
+        HashSet<NodeType> visitedNodes = new HashSet<NodeType>();
+
+        openQueue.Enqueue(startNode);
+        visitedNodes.Add(startNode);
+
+        int i; for (i = 0; i < maxiterations; ++i)
+        {
+            if (openQueue.Count == 0)
+            {
+                break;
+            }
+            else
+            {
+                NodeType searchFocus = openQueue.Dequeue();
+
+                // Explore neighbors
+                foreach (var neighbor in graph.Neighbors(searchFocus))
+                {
+                    if (visitedNodes.Contains(neighbor))
+                    {
+                        continue;
+                    }
+                    openQueue.Enqueue(neighbor);
+                    visitedNodes.Add(neighbor);
+                }
+            }
+        }
+        return visitedNodes.Count; // מחזיר את סך הצמתים הנגישים
+    }
+
+
+}
